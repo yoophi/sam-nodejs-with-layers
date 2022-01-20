b8decd1623f64ccf79be065c8fe49b7f8ad468cd
Pyunghyuk Yoo <yoophi@gmail.com>
2022-01-20 20:20:22 +0900

    ✨ NEW: lambda function

--- temp-units-conv/hello-world/app.js
+++ temp-units-conv/hello-world/app.js
@@ -1,33 +1,30 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+const tuc = require("temp-units-conv");
 let response;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+const scales = {
+  c: "celsius",
+  f: "fahrenheit",
+  k: "kelvin",
+};
 
-    return response
+exports.lambdaHandler = async (event, context) => {
+  let conversion = event.pathParameters.conversion;
+  let originalValue = event.pathParameters.value;
+  let answer = tuc[conversion](originalValue);
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        source: scales[conversion[0]],
+        target: scales[conversion[2]],
+        original: originalValue,
+        answer: answer,
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
 };
